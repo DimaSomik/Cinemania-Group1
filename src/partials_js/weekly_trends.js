34e0650d38e57fa1f20efd8f241529aa774636fa
Dima Somik <ghubdmytrosomik@gmail.com>
2024-10-18 11:36:03 +0200

    Leave comment in weekly_trends

--- src/partials_js/weekly_trends.js
+++ src/partials_js/weekly_trends.js
@@ -63,12 +63,13 @@ function displayMovies() {
       const movieCard = document.createElement('div');
       movieCard.classList.add('movie-card');
 
+      // Spróbuj tutaj h2 i p owinąć kolejnym div'em, i w stylach usunąć flex direction: column. 
       movieCard.innerHTML = `
         <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${
         movie.title
       }">
         <div class="movie-info">
-          <h2 class="movie-title">${movie.title}</h2>
+          <h2 class="movie-title">${movie.title}</h2> 
           <p>${genreNames} | ${movie.release_date.split('-')[0]}</p>
           <div class="stars">${getStars(movie.vote_average)}</div>
         </div>
